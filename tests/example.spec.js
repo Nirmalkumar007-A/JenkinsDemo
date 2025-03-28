e2b9fef4b6a1789758e179ca7cdfd109c32ce4c1
nirmalkumar007-a <a.nirmalkumar007@gmail.com>
2025-03-28 17:31:50 +0530

    added comment in nirmal/b

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -3,14 +3,14 @@ import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
   await page.goto('https://playwright.dev/');
-// changed 123
+// changed 124
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwrighs/);
 });
 /*
 test.skip('get started link', async ({ page }) => {
   await page.goto('https://playwright.dev/');
-
+ 
   // Click the get started link.
   await page.getByRole('link', { name: 'Get started' }).click();
 
